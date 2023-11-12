66e18e13d02a1eca9e7129ea8982684100bca8db
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2023-11-12 15:40:47 +0100

    some css

--- views/layout/header.php
+++ views/layout/header.php
@@ -6,8 +6,22 @@
   <title>NFL</title>
   <link rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+  <style>
+    body, html {
+      height: 100%;
+      margin: 0;
+    }
+      body {
+        background-image: url("https://www.tutorialspoint.com/images/logo.png");
+        background-repeat: no-repeat;
+        background-size: cover;
+        /* background-color: red; */
+      }
+  </style>
+
 </head>
-<body class="d-flex flex-column min-vh-100 p-3 mb-2 bg-dark text-light ">
-  <header>
+<body class="d-flex flex-column min-vh-100 p-3 mb-2">
+  <header class="ahmed">
     <h1 class="display-4 text-center">NFL</h1>
   </header>
